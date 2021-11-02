4327e794b67ec71caaaff0a83af7c10f1d624127
srikar45 <ksreddy4599@gmail.com>
2021-11-02 14:34:33 -0400

    hhgg

--- hapi-server-v1.0.10/metadata/INTERMAGNET/rsync.sh
+++
@@ -1,7 +0,0 @@
-# Push
-rsync -avz meta mag.gmu.edu:/var/www/git-data/server-nodejs/metadata/INTERMAGNET
-rsync -avz ftp.seismo.nrcan.gc.ca weigel@mag.gmu.edu:/var/www/git-data/server-nodejs/metadata/INTERMAGNET
-
-# Pull 
-#rsync -avz mag.gmu.edu:/var/www/git-data/server-nodejs/metadata/INTERMAGNET/meta .
-#rsync -avz weigel@mag.gmu.edu:/var/www/git-data/server-nodejs/metadata/INTERMAGNET/ftp.seismo.nrcan.gc.ca 
